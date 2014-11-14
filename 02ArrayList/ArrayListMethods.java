df868a94e950fc88f69b1f82a5f9be37c3fdf13c
jackye315 <jackye315@gmail.com>
2014-11-13 18:42:45 -0800

    Added static randomize, used random class

--- 02ArrayList/ArrayListMethods.java
+++ 02ArrayList/ArrayListMethods.java
@@ -1,7 +1,8 @@
 import java.util.*;
-public class ArrayDrive{
-    // ArrayList<Integer> al=new ArrayList<Integer>();
-    public void collapseDup(ArrayList<Integer> L){
+import java.util.Random;
+public class ArrayListMethods{
+    
+    public static void collapseDup(ArrayList<Integer> L){
     int x=0;
     while (x<L.size()-1){
         if (L.get(x)==L.get(x+1)){
@@ -12,12 +13,23 @@ public class ArrayDrive{
 	}
     }
     }
+    Random rand = new Random();
     
+    public static void randomize (ArrayList<Integer> A){
+	Random rand = new Random();
+	int x=0;
+	while (x<A.size()-1){
+	    int r = rand.nextInt(A.size());
+	    int r1=rand.nextInt(A.size());
+	    A.add(r,(A.remove(r1)));
+	    x=x+1;
+	}
+    }
 
     public static void main(String args[]){
 	ArrayList<Integer> a=new ArrayList<Integer>();
 	ArrayList<Integer> list=new ArrayList<Integer>();
-	ArrayDrive b=new ArrayDrive();
+	ArrayListMethods b=new ArrayListMethods();
 	
 a.add(1); 
 a.add(1);
@@ -36,9 +48,14 @@ a.add(4);
 a.add(5); 
 a.add(5);
 a.add(5); 
-a.add(5); 
+a.add(5);
+a.add(6);
+a.add(7);
+a.add(8); 
 System.out.println(a);
 b.collapseDup(a);
+System.out.println(a);
+b.randomize(a);
 System.out.println(a);
 
 
@@ -48,3 +65,9 @@ System.out.println(a);
 
 
 }
+
+
+
+
+
+

f81aea7cc632446e538b5f61ba3180fab1871ec0
Jack Ye <jackye315@gmail.com>
2014-12-02 20:29:05 -0800

    Did add

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -1,13 +1,21 @@
 public class OrderedSuperArray extends SuperArray{
 
+    public OrderedSuperArray(int x){
+	data=new String[x];
+	numArray=x;
+    }
+
     public void add(String o){
-	super.add(o);
 	int x=0;
 	while(x<data.length){
-	    if(data[x].compareTo(o)>0){
-		super.add(x,o);
-		super.remove(data.length);
+	    if(data[x]==null){
 		x=x+data.length;
+		super.add(o);
+		return;
+	    }
+	    else if(data[x].compareTo(o)>0){
+		super.add(x,o);
+		return;
 	    }
 	    x=x+1;
 	}
@@ -21,8 +29,17 @@ public class OrderedSuperArray extends SuperArray{
 
     public static void main (String []args){
 	OrderedSuperArray a;
-	a=new OrderedSuperArray();
+	a=new OrderedSuperArray(6);
+	System.out.println(a.size());
+	System.out.println(a);
+       	a.add("hi");
 	a.add("hi");
+	a.add("hi");
+	a.add("hi");
+	a.add("YO");
+	a.add("a");
+	a.add("A");
+	System.out.println(a);
 
 
 
